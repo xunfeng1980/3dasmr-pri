616033af12e01042bf3ed7a3d06a841691b6e6a6
xunfeng <xunfeng1980@gmail.com>
2025-11-22 21:50:30 +0800

    add macos app packaging and signing

--- pack.sh
+++ pack.sh
@@ -1,2 +1,22 @@
 uv add nuitka zstandard
 python -m nuitka --onefile --windows-disable-console --enable-plugin=pyside6,numpy --follow-import-to=need --output-dir=output --include-data-dir=assets=assets main.py
+
+
+python -m nuitka \
+  --standalone \
+  --macos-create-app-bundle \
+  --enable-plugin=pyside6,numpy \
+  --macos-app-name="3D ASMR" \
+  --company-name="xeelee.ai" \
+  --macos-app-version="1.0.0" \
+  --macos-signed-app-name="ai.xeelee.threedasmr" \
+  --macos-sign-identity="Q6559HL64L" \
+  --macos-sign-notarization \
+  --assume-yes-for-downloads \
+  --output-filename="3DASMR" \
+  --macos-app-icon="AppIcons/appstore.icns" \
+  --follow-import-to=need \
+  --output-dir=output \
+  --include-data-dir=assets=assets  \
+  --macos-app-protected-resource="NSMicrophoneUsageDescription:Need microphone access for ASMR recording." \
+  --script-name=main.py
