ee08154628932d4b44aaaab025f5f049eb1871c6
xunfeng <xunfeng1980@gmail.com>
2025-11-23 18:29:54 +0800

    fix windows packaging script

--- pack-win.sh
+++ pack-win.sh
@@ -2,9 +2,8 @@
 echo "Building 3D ASMR for Windows..."
 
 python -m nuitka \
-  --standalone \
+  --onefile \
   --enable-plugin=pyside6,numpy \
-  --include-data-dir=assets=assets \
   --output-filename="3DASMR" \
   --assume-yes-for-downloads \
   --output-dir=output \
